1d791315854be972ba83a68193e14d87d56d1838
Gabriela França <gabifmouraroc@outlook.com>
2024-04-12 15:18:28 -0300

    catálogo de carros

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface CustomFilterProps {
+    title: string
+}
+
+const CustomFilter = ({title}: CustomFilterProps) => {
+  return (
+    <div>
+      CustomFilter
+    </div>
+  )
+}
+
+export default CustomFilter
