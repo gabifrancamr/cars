900ca99758b50b818a98c37560c01352881ece9c
Gabriela França <gabifmouraroc@outlook.com>
2024-04-16 07:38:31 -0300

    rapid api cars and card details modal

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,10 +7,13 @@ interface CustomButtonProps {
     title: string
     containerStyles?: string
     handleClick?: MouseEventHandler<HTMLButtonElement>
-    btnType?: "button" | "submit" | "reset"
+    btnType?: "button" | "submit" 
+    textStyles?: string
+    rightIcon?: string
+    isDisabled?: boolean
 }
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button 
         disabled={false} 
@@ -18,9 +21,14 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomBu
         className={`custom-btn ${containerStyles}`} 
         onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image src={rightIcon} alt="right icon" fill className='object-contain' />
+          </div>
+        )} 
     </button> 
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -3,8 +3,11 @@ import { MouseEventHandler } from "react"
 export interface CustomButtonProps {
     title: string
     containerStyles?: string
-    handleClick: MouseEventHandler<HTMLButtonElement>
-    btnType?: "button" | "submit" | "reset"
+    handleClick?: MouseEventHandler<HTMLButtonElement>
+    btnType?: "button" | "submit" 
+    textStyles?: string
+    rightIcon?: string
+    isDisabled?: boolean 
 }
 
 export interface CustomFilterProps {
@@ -15,3 +18,58 @@ export interface SearchManufacturerProps {
     manufacturer: string
     setManufacturer: React.Dispatch<React.SetStateAction<string>>
 }
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
+
+export interface CardCarProps {
+    model: string;
+    make: string;
+    mpg: number;
+    transmission: string;
+    year: number;
+    drive: string;
+    cityMPG: number;
+}
+
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+  }
+  
+  export interface OptionProps {
+    title: string;
+    value: string;
+  }
+  
+  export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+  }
+  
+  export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+  }
+  
+  export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
